7dfe19517614f44b4e56b871b920613f69d6942e
yuto040308 <yuto.ge6@gmail.com>
2019-09-20 16:50:52 +0900

    countdown button maked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,9 @@
 //= require turbolinks
 //= require_tree .
 
+
+
+
 //
 // アセットパイプラインの仕様で、最初にjavascriptが走ってから、
 // HTMLが読まれるようだ。
@@ -24,9 +27,13 @@
 // なので、HTMLが読まれた後に表示するイベントリスナーに
 // HTMLの操作コードを記述する必要がある。
 // 
+
+// カウントダウン数を保持する変数
+let countdown = 0;
+
 (function() {
     document.addEventListener('DOMContentLoaded', function(e) {
-        var date, hour, label, minutes, seconds;
+        let date, hour, label, minutes, seconds;
 
         date = new Date();
         
@@ -40,13 +47,18 @@
         
         label.innerHTML = hour + "時" + minutes + "分" + seconds + "秒" + 'こんにちはああああ';
 
+        // ポモロード数の表示（初回の0のみ）
+        pomonum = document.querySelector('#pomonum');
+
+        pomonum.innerHTML = countdown; 
+
         
     });
 })();
 
 // setIntervalは指定の時間に指定の関数を実行することができる。
 function showClock1() {
-    var date, hour, label, minutes, seconds;
+    let date, hour, label, minutes, seconds;
 
     date = new Date();
 
@@ -61,3 +73,61 @@ function showClock1() {
     label.innerHTML = hour + "時" + minutes + "分" + seconds + "秒" + 'こんにちはああああ';
 }
 setInterval('showClock1()',1000);
+
+
+
+
+// ボタンが押されたら、カウントダウンする処理
+function OnButtonClick() {
+
+    // 
+    // カウントダウン処理
+    //
+    const totalTime = 10000;
+    const oldTime = Date.now();
+
+    // アロー関数を使用
+    const timerId = setInterval( () => {
+        const currentTime = Date.now();
+        // 差分を求める
+        const diff = currentTime - oldTime;
+
+        // 残りミリ秒を計算する
+        const remainMSec = totalTime - diff;
+        // ミリ秒を整数の秒数に変換する
+        const remainSec = Math.ceil(remainMSec / 1000);
+
+        let label =  "残り" + remainSec + "秒";
+
+
+        // 0秒以下になったら
+        if(remainSec <= 0){
+            // タイマーの終了の文言を表示
+            // letがスコープ外になっちゃってる。見れてない
+            label = "終了";
+
+            // 画面に表示する
+            document.querySelector('#timer').innerHTML = label;
+
+            // カウントダウン数を加算
+            countdown++;
+
+            // ポモロード数の更新
+            pomonum = document.querySelector('#pomonum');
+
+            pomonum.innerHTML = countdown;
+
+            // タイマーを終了する
+            clearInterval(timerId);
+
+            
+        }else{
+            // 画面に表示する
+            document.querySelector('#timer').innerHTML = label;
+        }
+
+        
+
+    }, 1000);
+}
+
